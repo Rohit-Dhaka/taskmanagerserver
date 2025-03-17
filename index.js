d83e10bafe2368d49b77d1eefadfd8a6258639fd
ankur-rajliwal0 <ankurflp@gmail.com>
2025-03-17 14:52:50 +0530

    add coment and morgan added

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const dotenv = require('dotenv')
-dotenv.config()
-const PORT = process.env.PORT || 3000
-const ConnectDb = require('./db/Connect.js')
-const routes = require('./routes/index.js')
-
-
-
-app.use(express.json())
-app.use(cors())
-app.use('/api/v1' ,routes)
-
-
-
-
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const morgan = require("morgan");
+const ConnectDb = require("./db/Connect.js");
+const routes = require("./routes/index.js");
+//Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1", routes);
+// DataBase
 ConnectDb();
-app.listen(PORT,()=>{
-    console.log(`App listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
