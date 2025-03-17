14df326cc9066e2c9600470dd78ed8121cc865d1
ankur-rajliwal0 <ankurflp@gmail.com>
2025-03-17 15:10:16 +0530

    add deadline in task model

--- models/Task.js
+++ models/Task.js
@@ -1,27 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true        
-    },
-    description:{
-        type:String,
-        required:true   
-    },
-    status:{
-        type:String,
-        enum:['Pending' , 'In Progress','Completed'],
-        default:'Pending'
-    },
-    // createdAt:{
-    //     type:Date,
-    //     default:Date.now        
-    // },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending",
+  },
+  deadline: {
+    type: Date,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
-const Task = mongoose.model("Task", taskSchema)
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
