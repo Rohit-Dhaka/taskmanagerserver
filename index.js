8fcf6a357739c59fd59c1f555c464b621f3f747b
ankur-rajliwal0 <ankurflp@gmail.com>
2025-03-20 10:41:14 +0530

    thi si s comet

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ const PORT = process.env.PORT || 3000;
 const morgan = require("morgan");
 const ConnectDb = require("./db/Connect.js");
 const routes = require("./routes/index.js");
-//Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
-
 app.use(cors());
 app.use("/api/v1", routes);
-// DataBase
 ConnectDb();
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
