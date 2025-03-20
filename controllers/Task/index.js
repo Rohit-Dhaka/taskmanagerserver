3452bf0509e5f4458867fa7dc13c106fa1bbece0
ankur-rajliwal0 <ankurflp@gmail.com>
2025-03-20 12:12:17 +0530

    nothinihih

--- controllers/Task/index.js
+++ controllers/Task/index.js
@@ -1,3 +1,3 @@
-const {addtask} = require('./addtask.js')
-const {getTasks} = require('./gettask.js')
-module.exports ={addtask ,getTasks}
+const { addtask } = require("./addtask.js");
+const { getUserTasks } = require("./gettask.js");
+module.exports = { addtask, getUserTasks };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const taskRoutes = express.Router();
-const { addtask ,getTasks } = require("../controllers/Task/index.js");
-const verifyToken = require("../middlewares/VerifyToken.js")
+const { addtask, getUserTasks } = require("../controllers/Task/index.js");
+const verifyToken = require("../middlewares/VerifyToken.js");
 
 taskRoutes.post("/addtask", verifyToken, addtask);
-taskRoutes.get("/taskget", verifyToken, getTasks);
+taskRoutes.get("/taskget", verifyToken, getUserTasks);
 
 module.exports = taskRoutes;
